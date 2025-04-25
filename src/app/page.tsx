caed818b549fc3138981e9724ce25ffd8012b6c4
Jonathan BOUS <j.bous@pyramide.lan>
2025-04-25 17:03:56 +0200

    Nos services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import img_accueil from "@/app/assets/img/img_accueil.png";
+import dev_web from "@/app/assets/img/dev_web.svg";
+import logiciel_metier from "@/app/assets/img/logiciel_metier.svg";
+import social_media from "@/app/assets/img/social_media.svg";
 import Image from "next/image";
 import styles from "@/app/styles/page.module.css";
 
@@ -57,7 +60,7 @@ export default function Home() {
 
                 <Image src={img_accueil} alt="Logo TechFlow" width={716} height={409} className={styles.img_accueil}/>
             </section>
-            <section id="services" style={{padding: '6rem 2rem', backgroundColor: '#fff'}}>
+            <section id="services" style={{ padding: '6rem 2rem', backgroundColor: '#fff' }}>
                 <h2 style={{
                     fontSize: '3rem',
                     fontWeight: 600,
@@ -75,15 +78,15 @@ export default function Home() {
                     margin: '0 auto',
                 }}>
                     {/* Bloc 1 */}
-                    <div style={{textAlign: 'center'}}>
+                    <div style={{ textAlign: 'center' }}>
                         <Image
-                            src="/services/dev_web.svg"
+                            src={dev_web}
                             alt="Développement Web & Mobile"
                             width={200}
                             height={200}
-                            style={{marginBottom: '1rem'}}
+                            style={{ marginBottom: '1rem' }}
                         />
-                        <h3 style={{fontSize: '1.5rem', marginBottom: '1rem'}}>Développement Web & Mobile</h3>
+                        <h3 style={{ fontSize: '1.5rem', marginBottom: '1rem' }}>Développement Web & Mobile</h3>
                         <p>
                             Création de sites vitrines, e-commerce ou applications web/mobile personnalisées,
                             performantes et modernes. Adaptées à vos besoins, optimisées SEO et responsives.
@@ -91,15 +94,15 @@ export default function Home() {
                     </div>
 
                     {/* Bloc 2 */}
-                    <div style={{textAlign: 'center'}}>
+                    <div style={{ textAlign: 'center' }}>
                         <Image
-                            src="/services/logiciel_metier.svg"
+                            src={logiciel_metier}
                             alt="Logiciels métiers"
                             width={200}
                             height={200}
-                            style={{marginBottom: '1rem'}}
+                            style={{ marginBottom: '1rem' }}
                         />
-                        <h3 style={{fontSize: '1.5rem', marginBottom: '1rem'}}>Développement de logiciels métiers</h3>
+                        <h3 style={{ fontSize: '1.5rem', marginBottom: '1rem' }}>Développement de logiciels métiers</h3>
                         <p>
                             Outils internes, gestion, automatisation, CRM, ERP… des solutions conçues sur mesure
                             pour simplifier votre quotidien professionnel.
@@ -107,15 +110,15 @@ export default function Home() {
                     </div>
 
                     {/* Bloc 3 */}
-                    <div style={{textAlign: 'center'}}>
+                    <div style={{ textAlign: 'center' }}>
                         <Image
-                            src="/services/social_media.svg"
+                            src={social_media}
                             alt="Réseaux sociaux & stratégie digitale"
                             width={200}
                             height={200}
-                            style={{marginBottom: '1rem'}}
+                            style={{ marginBottom: '1rem' }}
                         />
-                        <h3 style={{fontSize: '1.5rem', marginBottom: '1rem'}}>Stratégie digitale & réseaux sociaux</h3>
+                        <h3 style={{ fontSize: '1.5rem', marginBottom: '1rem' }}>Stratégie digitale & réseaux sociaux</h3>
                         <p>
                             Développement de votre présence en ligne : création de contenu, animation de comptes,
                             gestion de communauté, planification stratégique.
@@ -123,6 +126,7 @@ export default function Home() {
                     </div>
                 </div>
             </section>
+
 
         </>
 
